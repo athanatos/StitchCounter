b90775fc47a60d403198c6ee184ea5e99201250f
Samuel Just <sjust@redhat.com>
2015-06-14 13:18:26 -0700

    First commit

---
+++ app/src/main/java/com/sam/stitchcounter/SimpleCounterFragment.java
@@ -0,0 +1,23 @@
+package com.sam.stitchcounter;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class SimpleCounterFragment extends Fragment {
+
+    public SimpleCounterFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_simple_counter, container, false);
+    }
+}
